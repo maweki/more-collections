598775cf8b92d3d4fb6e8f8580a5e47e0c79f121
Mario Wenzel <maweki@gmail.com>
2015-09-16 16:02:39 +0200

    nicer implementation for contains for pure dicts

--- more_collections/puredict.py
+++ more_collections/puredict.py
@@ -19,7 +19,7 @@ def insert(parent, key, value):
     class __puredict(puredict_base):
         __len__ = lambda _: len(parent) + (0 if (key in parent) else 1)
         __getitem__ = lambda _, _key: value if (key == _key) else parent[_key]
-        __contains__ = lambda _, _key: True if (key == _key) else (_key in parent)
+        __contains__ = lambda _, _key: (key == _key) or (_key in parent)
         __iter__ = lambda _: chain((key,), (k for k in parent if not key == k))
         items = lambda _: chain(((key, value),), ((k, v) for (k, v) in parent.items() if not key == k))
     return __puredict()
@@ -28,7 +28,7 @@ def delete(parent, key):
     class __puredict(puredict_base):
         __len__ = lambda _: len(parent) - (1 if (key in parent) else 0)
         __getitem__ = lambda _, _key: _raise(KeyError()) if (key == _key) else parent[_key]
-        __contains__ = lambda _, _key: False if (key == _key) else (_key in parent)
+        __contains__ = lambda _, _key: (not key == _key) and (_key in parent)
         __iter__ = lambda _: (_key for _key in parent if not key == _key)
         items = lambda _: ((k, v) for (k, v) in parent.items() if not key == k)
     return __puredict()
