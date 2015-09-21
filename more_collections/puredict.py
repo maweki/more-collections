e0e749212068fc4ee5e4ed8cca3d54847157d785
Mario Wenzel <maweki@gmail.com>
2015-09-21 22:52:17 +0200

    better implementation for len in puredicts

--- more_collections/puredict.py
+++ more_collections/puredict.py
@@ -8,7 +8,6 @@ from .common import _raise
 
 def empty(_ = None):
     class __puredict(puredict_base):
-        __len__ = lambda _: 0
         __getitem__ = lambda _, __: _raise(KeyError())
         __contains__ = lambda _, __: False
         __iter__ = lambda _: iter(())
@@ -17,7 +16,6 @@ def empty(_ = None):
 
 def insert(parent, key, value):
     class __puredict(puredict_base):
-        __len__ = lambda _: len(parent) + (0 if (key in parent) else 1)
         __getitem__ = lambda _, _key: value if (key == _key) else parent[_key]
         __contains__ = lambda _, _key: (key == _key) or (_key in parent)
         __iter__ = lambda _: chain((key,), (k for k in parent if not key == k))
@@ -26,7 +24,6 @@ def insert(parent, key, value):
 
 def delete(parent, key):
     class __puredict(puredict_base):
-        __len__ = lambda _: len(parent) - (1 if (key in parent) else 0)
         __getitem__ = lambda _, _key: _raise(KeyError()) if (key == _key) else parent[_key]
         __contains__ = lambda _, _key: (not key == _key) and (_key in parent)
         __iter__ = lambda _: (_key for _key in parent if not key == _key)
@@ -37,6 +34,7 @@ class puredict_base(Mapping):
     empty = empty
     insert = insert
     delete = delete
+    __len__ = lambda self: sum(1 for _ in self.items())
     __hash__ = lambda self: hash(frozenset(self.items())) ^ hash(puredict_base)
     __repr__  = lambda self: repr(dict(self))
 
